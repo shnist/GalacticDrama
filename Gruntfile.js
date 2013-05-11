a1ec6084d6a8d41baf0dfef3d918c3c8149c84e0
aaron faber <shnist@btinternet.com>
2013-05-11 15:31:58 +0100

    Added ignore files and created grunt build including fpt deployment

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		clean: ['dist'],
+		copy: {
+			main: {
+				files: [
+					{
+						expand: true,
+						src: ['css/','js/','fonts/','images/','*.html','*.php','*.txt'],
+						dest: 'dist/'
+					}
+				]
+			}
+		},
+		'ftp-deploy': {
+			build: {
+				auth: {
+					host: 'ftp.mandygodding.co.uk',
+					port: 21,
+					authKey: 'galacticDramaKey'
+				},
+				src: 'dist/',
+				// .ftppass with details is needed (not included in repo)
+				dest: '/htdocs/galacticDrama/',
+				exclusions: ['dist/.DS_Store', 'dist/Thumbs.db', 'dist/tmp', 'dist/.git']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-ftp-deploy');
+
+	// Default task(s).
+	grunt.registerTask('default', ['clean','copy']);
+	grunt.registerTask('deploy', ['clean','copy','ftp-deploy']);
+};
